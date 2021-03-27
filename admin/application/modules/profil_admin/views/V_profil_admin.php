0fd40b75c404b5ddd4b5afe383f1e3b555708263
boyabdulhasan <boyabdulhasan@gmail.com>
2021-03-27 10:09:03 +0700

    profil admin
    update

--- admin/application/modules/profil_admin/views/V_profil_admin.php
+++ admin/application/modules/profil_admin/views/V_profil_admin.php
@@ -1,30 +1,128 @@
 <div class="app-main__inner">
-                    <div class="app-page-title">
-                        <div class="page-title-wrapper">
-                            <div class="page-title-heading">
-                                <div class="page-title-icon">
-                                    <i class="pe-7s-car icon-gradient bg-mean-fruit"></i>
-                                </div>
-                                <div>Halaman profil admin
-                                    <div class="page-title-subheading">Terdapat beberapa option untuk melihat keterangan</div>
+                    <div class="tabs-animation">
+                        <div class="row">
+                            <div class="col-md-12 col-lg-6">
+                                <div class="card-shadow-primary card-border mb-3 profile-responsive-sm card">
+                                    <div class="dropdown-menu-header">
+                                        <div class="dropdown-menu-header-inner bg-vicious-stance">
+                                            <div class="menu-header-image opacity-1"
+                                                style="background-image: url('assets/images/dropdown-header/abstract1.jpg');"></div>
+                                            <div class="menu-header-content btn-pane-right">
+                                                <div class="avatar-icon-wrapper mr-3 avatar-icon-xl btn-hover-shine">
+                                                    <div class="avatar-icon rounded-circle">
+                                                        <img src="assets/images/avatars/13.jpg" alt="Avatar 5">
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <h5 class="menu-header-title">Abdul Hasan</h5>
+                                                    <h6 class="menu-header-subtitle">Admin 1</h6>
+                                                </div>
+                                                <div class="menu-header-btn-pane">
+                                                    <a href="edit-profil-dosis.html">
+                                                    <button class="btn btn-success">Lihat</button>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-
-                            <div class="page-title-actions">
-                                 <button type="button" data-toggle="tooltip" title="Refresh" data-placement="bottom"
-                                    class="btn-shadow mr-3 btn btn-dark">
-                                    <i class="fa fa-spinner"></i>
-                                </button>
-                                
-                                <div class="d-inline-block dropdown">
-                                    <button class="btn btn-primary" id="reportrange">
-                                        <i class="fa fa-calendar pr-1"></i>
-                                         <span></span>
-                                        <i class="fa pl-1 fa-caret-down"></i>
-                                    </button>
-                                    
+                            <div class="col-md-12 col-lg-6">
+                                <div class="card-shadow-primary card-border mb-3 profile-responsive-sm card">
+                                    <div class="dropdown-menu-header">
+                                        <div class="dropdown-menu-header-inner bg-vicious-stance">
+                                            <div class="menu-header-image opacity-1"
+                                                style="background-image: url('assets/images/dropdown-header/abstract1.jpg');"></div>
+                                            <div class="menu-header-content btn-pane-right">
+                                                <div class="avatar-icon-wrapper mr-3 avatar-icon-xl btn-hover-shine">
+                                                    <div class="avatar-icon rounded-circle">
+                                                        <img src="assets/images/avatars/17.jpg" alt="Avatar 5">
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <h5 class="menu-header-title">Farhan Ariyanto</h5>
+                                                    <h6 class="menu-header-subtitle">Admin 2</h6>
+                                                </div>
+                                                <div class="menu-header-btn-pane">
+                                                    <button class="btn btn-success">Lihat</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div> 
+                            </div>
+                            <div class="col-md-12 col-lg-6">
+                                <div class="card-shadow-primary card-border mb-3 profile-responsive-sm card">
+                                    <div class="dropdown-menu-header">
+                                        <div class="dropdown-menu-header-inner bg-vicious-stance">
+                                            <div class="menu-header-image opacity-1"
+                                                style="background-image: url('assets/images/dropdown-header/abstract1.jpg');"></div>
+                                            <div class="menu-header-content btn-pane-right">
+                                                <div class="avatar-icon-wrapper mr-3 avatar-icon-xl btn-hover-shine">
+                                                    <div class="avatar-icon rounded-circle">
+                                                        <img src="assets/images/avatars/15.jpg" alt="Avatar 5">
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <h5 class="menu-header-title">Indah Novita</h5>
+                                                    <h6 class="menu-header-subtitle">Admin 3</h6>
+                                                </div>
+                                                <div class="menu-header-btn-pane">
+                                                    <button class="btn btn-success">Lihat</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-12 col-lg-6">
+                                <div class="card-shadow-primary card-border mb-3 profile-responsive-sm card">
+                                    <div class="dropdown-menu-header">
+                                        <div class="dropdown-menu-header-inner bg-vicious-stance">
+                                            <div class="menu-header-image opacity-1"
+                                                style="background-image: url('assets/images/dropdown-header/abstract1.jpg');"></div>
+                                            <div class="menu-header-content btn-pane-right">
+                                                <div class="avatar-icon-wrapper mr-3 avatar-icon-xl btn-hover-shine">
+                                                    <div class="avatar-icon rounded-circle">
+                                                        <img src="assets/images/avatars/18.jpg" alt="Avatar 5">
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <h5 class="menu-header-title">Sandra</h5>
+                                                    <h6 class="menu-header-subtitle">Admin 4</h6>
+                                                </div>
+                                                <div class="menu-header-btn-pane">
+                                                    <button class="btn btn-success">Lihat</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-12 col-lg-6">
+                                <div class="card-shadow-primary card-border mb-3 profile-responsive-sm card">
+                                    <div class="dropdown-menu-header">
+                                        <div class="dropdown-menu-header-inner bg-vicious-stance">
+                                            <div class="menu-header-image opacity-1"
+                                                style="background-image: url('assets/images/dropdown-header/abstract1.jpg');"></div>
+                                            <div class="menu-header-content btn-pane-right">
+                                                <div class="avatar-icon-wrapper mr-3 avatar-icon-xl btn-hover-shine">
+                                                    <div class="avatar-icon rounded-circle">
+                                                        <img src="assets/images/avatars/16.jpg" alt="Avatar 5">
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <h5 class="menu-header-title">Tiara Nur</h5>
+                                                    <h6 class="menu-header-subtitle">Admin 5</h6>
+                                                </div>
+                                                <div class="menu-header-btn-pane">
+                                                    <button class="btn btn-success">Lihat</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>  
+                    </div>
                 </div>
