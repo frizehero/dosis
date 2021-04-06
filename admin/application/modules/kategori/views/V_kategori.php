66295e33337a9f4acfe5f819345781b707b9efd9
boyabdulhasan <boyabdulhasan@gmail.com>
2021-04-06 10:03:28 +0700

    update kategori

--- admin/application/modules/kategori/views/V_kategori.php
+++ admin/application/modules/kategori/views/V_kategori.php
@@ -8,6 +8,137 @@
                                 <div>Kategori Donasi
                             </div>
                         </div>
+                    </div>            
+                        <ul class="body-tabs body-tabs-layout tabs-animated body-tabs-animated nav">
+                            <li class="nav-item">
+                                <a role="tab" class="nav-link active" id="tab-0" data-toggle="tab" href="#tab-content-0">
+                                    <span>Card Tabs</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a role="tab" class="nav-link" id="tab-1" data-toggle="tab" href="#tab-content-1">
+                                    <span>Animated Lines</span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a role="tab" class="nav-link" id="tab-2" data-toggle="tab" href="#tab-content-2">
+                                    <span>Basic</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="tab-content">
+                            <div class="tab-pane tabs-animation fade show active" id="tab-content-0" role="tabpanel">
+                                <div class="row">
+                                    <div class="col-sm-12 col-md-4">
+                                        <div class="widget-content p-0">
+                                            <div class="widget-content-outer">
+                                                <div class="widget-content-wrapper">
+                                                    <div class="widget-content-left">
+                                                        <div class="widget-numbers text-dark"></div>
+                                                    </div>
+                                                </div>
+                                                <div class="widget-progress-wrapper mt-1">
+
+                                                    <div class="progress-sub-label">
+                                                        <div class="sub-label-left font-size-md"></div>
+                                                    </div>
+                                                    <div>
+                                                        <br>
+                                                    </div>
+                                                    
+                                                    <div class="card-hover-shadow profile-responsive card-border border-success mb-3 card">
+                                                        <div class="dropdown-menu-header">
+                                                            <div class="wrapper rounded"></div>
+                                                            <img src="assets/images/Peristiwa/3.jpg" style="width: 314px; height:210px;">   
+                                                        </div>
+                                                        <div class="p-0 card-body">
+                                                            <div class="tab-content">
+                                                                <div class="tab-pane active show" id="tab-2-eg1">
+                                                                    <ul class="list-group list-group-flush">
+                                                                        <li class="list-group-item">
+                                                                            <div class="h5 modal-title">
+                                                                                <div class="text-right">
+                                                                                    <h6>
+                                                                                        10/10/2020
+                                                                                    </h6>
+                                                                                </div>
+                                                                                <h5 class="menu-header-title">
+                                                                                    <div>Bedah Sekolah dari kardus di pelosok Desa Kabupaten Probolinggo</div>
+                                                                                </h5>
+                                                                                <h6>
+                                                                            <div>Diposting oleh @boyabdulhasan</div>
+                                                                                </h6>
+                                                                            </div>
+                                                                        </li>
+                                                                        <li class="list-group-item">
+                                                                            Rp 0 Terkumpul dari Rp 15.000.000    
+                                                                            <div class="progress-bar-xs progress-bar-animated-alt progress">
+                                                                            <div class="progress-bar bg-info" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style="width: 65%;">
+                                                                            </div>
+                                                                            </div>
+                                                                            <div> <br> </div>
+                                                                            <div> Sisa Waktu : 0 Hari</div>
+                                                                            <div> 465 Orang telah Berdonasi</div>
+                                                                        </li>
+                                                                        <div class="card-body">
+                                                                        <div class="br-wrapper br-theme-css-stars"><select id="css-stars" style="display: none;">
+                                                                            <option value="1">1</option>
+                                                                            <option value="2">2</option>
+                                                                            <option value="3">3</option>
+                                                                            <option value="4">4</option>
+                                                                            <option value="5">5</option>
+                                                                        </select><div class="br-widget"><a href="#" data-rating-value="1" data-rating-text="1" class="br-selected br-current"></a><a href="#" data-rating-value="2" data-rating-text="2" class=""></a><a href="#" data-rating-value="3" data-rating-text="3"></a><a href="#" data-rating-value="4" data-rating-text="4"></a><a href="#" data-rating-value="5" data-rating-text="5"></a><div class="br-current-rating">1</div></div></div>
+                                                                    </div>
+
+                                                                    </ul>
+                                                                </div>
+                                                                <div class="tab-pane" id="tab-2-eg2">
+                                                                    <ul class="list-group list-group-flush"><a href="javascript:void(0);" class="disabled list-group-item">Cras justo odio</a><a href="javascript:void(0);" class="list-group-item">Dapibus ac facilisis in</a><a href="javascript:void(0);" class="list-group-item">Morbi leo risus</a><a href="javascript:void(0);" class="list-group-item">Porta ac consectetur ac</a><a href="javascript:void(0);" class="list-group-item">Vestibulum at eros</a>
+                                                                    </ul>
+                                                                </div>
+                                                                <div class="tab-pane" id="tab-2-eg3">
+                                                                    <div class="p-3">
+                                                                        <p>With supporting text below as a natural lead-in to additional content.</p>
+                                                                        <p class="mb-0">Lorem Ipsum has been the industry's standard dummy text ever since
+                                                                            the 1500s, when an unknown printer took a galley of type and scrambled.</p>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="text-center d-block card-footer">
+                                                            <a href="<?php echo base_url('detail_donasi'); ?>" class="btn-shadow-primary btn btn-primary btn-lg">Detail</a>
+                                                            <button type="button" class="btn-shadow-primary btn btn-primary btn-lg" data-toggle="modal" data-target=".bd-example-modal-sm">Bagikan</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane tabs-animation fade" id="tab-content-1" role="tabpanel">
+                                <div class="row">
+                                    Isi Ke dua
+                                </div>
+            
+                            </div>
+                            <div class="tab-pane tabs-animation fade" id="tab-content-2" role="tabpanel">
+                                <div class="row">
+                                    Isi Ke tiga
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+<div class="app-main__inner">
+                    <div class="app-page-title">
+                        <div class="page-title-wrapper">
+                            <div class="page-title-heading">
+                                <div class="page-title-icon">
+                                    <i class="pe-7s-star icon-gradient bg-ripe-malin"></i>
+                                </div>
+                                <div>Kategori Donasi
+                            </div>
+                        </div>
                     </div>        
                         <ul class="body-tabs body-tabs-layout tabs-animated body-tabs-animated nav">
                         <li class="nav-item">
@@ -73,7 +204,7 @@
                                                                 </div>
                                                                 <div class="p-0 card-body">
                                                                     <div class="tab-content">
-                                                                        <div class="tab-pane active show" id="tab-2-eg1">
+                                                                        <div class="tab-pane active show" id="tab-0-eg1">
                                                                             <ul class="list-group list-group-flush">
                                                                                 <li class="list-group-item">
                                                                                     <div class="h5 modal-title">
