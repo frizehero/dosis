d6fdba4031877d26f4c383dd37c89619d50f1604
sandralidya10 <lidyasandra2003@gmail.com>
2021-04-19 21:55:01 +0700

    faq dan kategori banjir
    edit

---
+++ user/application/modules/faq/views/V_faq.php
@@ -0,0 +1,77 @@
+<div class="content">
+<section class="parallax-section" data-scrollax-parent="true" id="sec1" style="height: 100px; margin-top: 80px;">
+                <div class="bg par-elem "  data-bg="images/bg/kategori.jpg" data-scrollax="properties: { translateY: '30%' }"></div>
+                <div class="overlay"></div>
+                <div class="container">
+                    <div class="section-title center-align" style="margin-top: -35px;"> 
+                        <h2><span><b>PUSAT BANTUAN</b></span></h2>
+                    </div>
+                </div>
+            </section>
+                <div class="list-single-main-item fl-wrap">
+                    <div class="row">
+                        <div class="col-md-3">
+                            Iklan
+                        </div>
+                        <div class="col-md-9">
+                        <p style="font-size: 30px;"><b>DONASI</b></P>
+                            <div class="accordion" style="padding-bottom: 30px;">
+                        <a class="toggle act-accordion" href="#"> Bagaimana cara berdonasi di dosis.com   <i class="fa fa-angle-down"></i></a>
+                        <div class="accordion-inner visible">
+                            <p>Berikut ini adalah : Silakan buka Dosis.com melalui browser HP atau PC Anda. Anda juga dapat menggunakan aplikasi Kitabisa yang bisa diunduh di Playstore dan iOS. Silakan memilih salah satu halaman galang dana yang diminati. Pada halaman galang dana tersebut, silakan klik tombol berwarna merah muda bertuliskan "DONASI SEKARANG". Silakan ikuti petunjuk yang diberikan sistem (mengisi nominal - pilih metode pembayaran - identitas diri). Pada akhir donasi online, terdapat rangkuman pembayaran yang berisi nomor rekening tujuan transfer dan kode unik yang harus dicantumkan pada nominal transfer. Mohon transfer tepat sesuai tagihan yang diinformasikan sistem, supaya donasi terverifikasi otomatis tanpa harus konfirmasi kembali kepada kami. Anda akan menerima notifikasi via sms dan/atau email begitu donasi terverifikasi.</p>
+                        </div>
+                        <a class="toggle" href="#"> Apakah saya harus memiliki akun untuk berdonasi? <i class="fa fa-angle-down"></i></a>
+                        <div class="accordion-inner">
+                            <p>Iya, Namun jika Anda membuat akun di dosis.com, Anda dapat:
+
+                                1. Melihat riwayat donasi
+                                Anda juga dapat melihat status donasi secara online.
+                                
+                                2. Menggunakan fasilitas dompet kebaikan
+                                Dengan Dompet Kebaikan, Anda dapat mengisi saldo dan donasi berkali-kali tanpa harus transfer bank setiap saat (hingga saldo milik Anda habis).</p>
+                        </div>
+                        <a class="toggle" href="#"> Apakah saya dapat mengubah data diri?  <i class="fa fa-angle-down"></i></a>
+                        <div class="accordion-inner">
+                            <p>Anda dapat mengubah data secara mandiri, kecuali nama lengkap, alamat email, dan nomor WhatsApp.
+
+                                Khusus untuk penggalang dana, nama lengkap harus sesuai dengan nama yang tertera di KTP untuk akun personal, atau sesuai NPWP untuk akun organisasi jika ingin akun Anda terverifikasi.
+                                
+                                WhatsApp merupakan sarana komunikasi utama Kitabisa kepada penggalang dana, namun kami menyarankan untuk tetap memasukkan email di akun Anda.
+                                Pastikan nomor WhatsApp dan email yang dimasukkan masih aktif Anda gunakan.</p>
+                        </div>
+                            </div>
+                        <p style="font-size: 30px;"><b>GALANG DANA</b></P>
+                            <div class="accordion">
+                        <a class="toggle act-accordion" href="#"> Panduan Menggalang Dana di dosis.com   <i class="fa fa-angle-down"></i></a>
+                        <div class="accordion-inner visible">
+                            <p>A. Setiap penggalang dana di Kitabisa wajib:</p></br>
+                            <p>1. Memberikan informasi yang benar
+                                Pastikan setiap informasi yang tertera di halaman galang dana Anda adalah benar, jujur dan dapat dibuktikan.
+                            <p>2. Menggunakan tata krama dan etika</br>
+                                Mengedepankan tata krama dan etika dalam memberikan informasi, menyebarkan serta mengajak orang-orang untuk berdonasi pada halaman galang dana. Contoh: tidak memaksa, tidak merugikan pihak lain dan tidak mengganggu / spam.</p>
+                            <p>3. Menjaga transparansi</br>
+                                Memberikan keterbukaan informasi yang benar pada halaman galang dana selama masa penggalangan dana berlangsung. Bersedia dihubungi untuk dimintai keterangan lebih lanjut oleh Kitabisa apabila diperlukan, serta menjaga transparansi terkait penggunaan dana yang telah terkumpul melalui fitur Tulis Kabar (update).</p>
+                        </div>
+                        <a class="toggle" href="#"> Apakah saya harus memiliki akun untuk berdonasi? <i class="fa fa-angle-down"></i></a>
+                        <div class="accordion-inner">
+                            <p>Iya, Namun jika Anda membuat akun di dosis.com, Anda dapat:
+
+                                1. Melihat riwayat donasi
+                                Anda juga dapat melihat status donasi secara online.
+                                
+                                2. Menggunakan fasilitas dompet kebaikan
+                                Dengan Dompet Kebaikan, Anda dapat mengisi saldo dan donasi berkali-kali tanpa harus transfer bank setiap saat (hingga saldo milik Anda habis).</p>
+                        </div>
+                        <a class="toggle" href="#"> Apakah saya dapat mengubah data diri?  <i class="fa fa-angle-down"></i></a>
+                        <div class="accordion-inner">
+                            <p>Anda dapat mengubah data secara mandiri, kecuali nama lengkap, alamat email, dan nomor WhatsApp.
+
+                                Khusus untuk penggalang dana, nama lengkap harus sesuai dengan nama yang tertera di KTP untuk akun personal, atau sesuai NPWP untuk akun organisasi jika ingin akun Anda terverifikasi.
+                                
+                                WhatsApp merupakan sarana komunikasi utama Kitabisa kepada penggalang dana, namun kami menyarankan untuk tetap memasukkan email di akun Anda.
+                                Pastikan nomor WhatsApp dan email yang dimasukkan masih aktif Anda gunakan.</p>
+                        </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
---
+++ user/application/modules/kategori_banjir/controllers/kategori_banjir.php
@@ -0,0 +1,64 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kategori_banjir extends MX_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		// model
+		 $this->load->model('m_kategori_banjir');
+		 $this->load->model('login/m_session');
+	}
+
+	
+	// index
+	function index()
+	{
+
+		if ( empty( $this->session->userdata('session_id') ) )
+		{
+			redirect('login');
+
+		} else {
+
+			$data = array(
+			'namamodule' 	=> "kategori_banjir",
+			'namafileview' 	=> "V_kategori_banjir",
+		);
+		echo Modules::run('template/tampilBeranda', $data);
+		}
+
+
+	}
+
+	function tambah()
+	{
+		$this->m_data_sekolah->tambah();
+		redirect('data_sekolah');
+	}
+
+	function edit()
+	{
+		$this->m_data_sekolah->edit();
+		redirect('data_sekolah');
+	}
+
+	function hapus($id)
+	{
+		$this->m_data_sekolah->hapus($id);
+		redirect('data_sekolah');
+	}
+
+	function cari()
+	{
+		$data = array(
+			'namamodule' 	=> "data_sekolah",
+			'namafileview' 	=> "V_data_sekolah",
+			'tampil'		=> $this->m_data_sekolah->cari(),
+		);
+		echo Modules::run('template/tampilCore', $data);
+	}
+	
+}
+ 
