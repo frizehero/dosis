3ee04dc3d6095506e3ad79e700cf05d73fc36f8b
tiaranh_ <tiarahasanah5055@gmail.com>
2021-04-19 21:52:01 +0700

    user-hubungi_kami

---
+++ user/application/modules/hubungi_kami/views/V_hubungi_kami.php
@@ -0,0 +1,100 @@
+<div class="content">
+                    <section class="parallax-section" data-scrollax-parent="true">
+                        <div class="bg par-elem " data-bg="images/bg/2.jpg" data-scrollax="properties: { translateY: '30%' }" style="background-image: url(&quot;images/bg/2.jpg&quot;); transform: translateZ(0px) translateY(24.9231%);"></div>
+                        <div class="overlay"></div>
+                        <div class="bubble-bg"><div class="bubbles"><div class="individual-bubble" style="left: 183px; width: 15px; height: 15px; opacity: 0.00111392;"></div><div class="individual-bubble" style="left: 519px; width: 5px; height: 5px; opacity: 0.0107751;"></div><div class="individual-bubble" style="left: 259px; width: 15px; height: 15px; opacity: 0.017785;"></div><div class="individual-bubble" style="left: 488px; width: 15px; height: 15px; opacity: 0.0358584;"></div><div class="individual-bubble" style="left: 1157px; width: 15px; height: 15px; opacity: 0.0567845;"></div><div class="individual-bubble" style="left: 471px; width: 10px; height: 10px; opacity: 0.0780125;"></div><div class="individual-bubble" style="left: 1281px; width: 10px; height: 10px; opacity: 0.10802;"></div><div class="individual-bubble" style="left: 110px; width: 20px; height: 20px; opacity: 0.144077;"></div><div class="individual-bubble" style="left: 255px; width: 10px; height: 10px; opacity: 0.205312;"></div><div class="individual-bubble" style="left: 953px; width: 5px; height: 5px; opacity: 0.230521;"></div><div class="individual-bubble" style="left: 549px; width: 20px; height: 20px; opacity: 0.28125;"></div><div class="individual-bubble" style="left: 882px; width: 20px; height: 20px; opacity: 0.357858;"></div><div class="individual-bubble" style="left: 353px; width: 20px; height: 20px; opacity: 0.391436;"></div><div class="individual-bubble" style="left: 1086px; width: 10px; height: 10px; opacity: 0.454295;"></div></div></div>
+                        <div class="container">
+                            <div class="section-title center-align">
+                                <h2><span>Hubungi Kami</span></h2>
+                            </div>
+                        </div>
+                        <div class="header-sec-link">
+                            <div class="container"><a href="#sec1" class="custom-scroll-link">Let's Start</a></div>
+                        </div>
+                    </section>
+                    <!-- section end -->
+                    <!--section -->  
+                    <section id="sec1">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="list-single-main-item fl-wrap">
+                                        <div class="list-single-main-item-title fl-wrap">
+                                            <h3>Detail<span> Kontak</span></h3>
+                                        </div>
+                                        <div class="list-single-main-media fl-wrap">
+                                            <img src="images/all/new/15.jpg" class="respimg" alt="">
+                                        </div>
+                                        <p>Ut euismod ultricies sollicitudin. Curabitur sed dapibus nulla. Nulla eget iaculis lectus. Mauris ac maximus neque. Nam in mauris quis libero sodales eleifend. Morbi varius, nulla sit amet rutrum elementum, est elit finibus tellus, ut tristique elit risus at metus. In ut odio libero, at vulputate urna. Nulla tristique mi a massa convallis cursus. Nulla eu mi magna. Etiam suscipit commodo gravida.</p>
+                                        <div class="list-author-widget-contacts">
+                                            <ul>
+                                                <li><span><i class="fa fa-map-marker"></i> Alamat  :</span> <a href="#">JL Soekarno Hatta no.1 Karangploso,Kabupaten Malang</a></li>
+                                                <li><span><i class="fa fa-phone"></i> Telepon :</span> <a href="#">(0335) 7789 2736</a></li>
+                                                <li><span><i class="fa fa-envelope-o"></i> E-Mail   :</span> <a href="#">donasiswa2020@gmail.com</a></li>
+                                                <li><span><i class="fa fa-globe"></i> Website :</span> <a href="#">Dosis.com</a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="list-widget-social">
+                                            <ul>
+                                                <li><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                                                <li><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                                                <li><a href="#" target="_blank"><i class="fa fa-vk"></i></a></li>
+                                                <li><a href="#" target="_blank"><i class="fa fa-whatsapp"></i></a></li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="list-single-main-wrapper fl-wrap">
+                                        <div class="list-single-main-item-title fl-wrap">
+                                            <h3>Lokasi Kami</h3>
+                                        </div>
+                                        <div class="map-container">
+                                            <div id="singleMap" data-latitude="40.7427837" data-longitude="-73.11445617675781"><div style="height: 100%; width: 100%;"></div></div>
+                                        </div>
+                                        
+                                            <form class="custom-form" action="http://citybook.kwst.net/php/contact.php" name="contactform" id="contactform">
+                                                <fieldset>
+                                                    <div class="col-md-6">
+                                                        <label><i class="fa fa-map-marker"></i></label>
+                                                        <input type="text" name="name" id="name" placeholder="Latitude *" value="">
+                                                        <div class="clearfix"></div>
+                                                    </div>
+                                                    <div class="col-md-6">
+                                                        <label><i class="fa fa-map-marker"></i>  </label>
+                                                        <input type="text" name="email" id="email" placeholder="Longitude *" value="">
+                                                    </div>
+                                                </fieldset>
+                                            </form>
+
+                                        <span class="fw-separator"></span>                       
+                                        
+                                        <div class="list-single-main-item-title fl-wrap">
+                                            <h3>Kirim Pesan</h3>
+                                        </div>
+                                        <div id="contact-form">
+                                            <div id="message"></div>
+                                            <form class="custom-form" action="http://citybook.kwst.net/php/contact.php" name="contactform" id="contactform">
+                                                <fieldset>
+                                                    <label><i class="fa fa-user-o"></i></label>
+                                                    <input type="text" name="name" id="name" placeholder="Nama *" value="">
+                                                    <div class="clearfix"></div>
+                                                    <label><i class="fa fa-envelope-o"></i>  </label>
+                                                    <input type="text" name="email" id="email" placeholder="Email *" value="">
+                                                    <textarea name="comments" id="comments" cols="40" rows="3" placeholder="Pesan:"></textarea>
+                                                </fieldset>
+                                                <button class="btn  big-btn  color-bg flat-btn" id="submit">Kirim<i class="fa fa-angle-right"></i></button>
+                                            </form>
+                                        </div>
+                                        <!-- contact form  end--> 
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <!-- section end -->
+                    <div class="limit-box fl-wrap"></div>
+                    <!--section -->  
+                    
+                    <!-- section end -->
+                </div>
