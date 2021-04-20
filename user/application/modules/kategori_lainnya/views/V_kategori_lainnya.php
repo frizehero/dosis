230ddf045b5fec8323c60036cb722d10f7aa97ba
boyabdulhasan <boyabdulhasan@gmail.com>
2021-04-20 08:29:03 +0700

    update tampilan & beranda user

---
+++ user/application/modules/kategori_lainnya/views/V_kategori_lainnya.php
@@ -0,0 +1,152 @@
+<div class="content">
+                    <!--section -->
+                    <section class="parallax-section" data-scrollax-parent="true" id="sec1" style="height: 100px;">
+                        <div class="bg par-elem "  data-bg="images/bg/kategori.jpg" data-scrollax="properties: { translateY: '30%' }"></div>
+                        <div class="container">
+                            <div class="section-title center-align" style="margin-top: -35px;"> 
+                                <h2><span><b>KATEGORI</b></span></h2>
+                                <p>Pilih Kategori yang diinginkan</p>
+                            </div>
+                        </div>
+                    </section>
+                    <!-- section end -->
+                    <!--section -->
+                    <div class="col-list-wrap fh-col-list-wrap left-list">
+                        <!-- carousel -->
+                        <div class="list-main-wrap fl-wrap card-listing" style="padding: 30px 10px 0px 45px;">
+                            <!-- listing-item -->
+                            <div class="listing-item" style="width: 315px; padding: 30px 30px 0px 30px;"><a href="kategori-putus-sekolah.html">
+                                <article class="geodir-category-listing fl-wrap">
+                                    <div class="geodir-category-img">
+                                        <img src="images/kategori/putus.jpg" alt="">
+                                    </div>
+                                </article>
+                                <div class="section-title" style="margin-top: 20px;">
+                                    <h4><b>SISWA PUTUS SEKOLAH</b></h4>
+                                </div></a>
+                            </div>
+                            <!-- listing-item end-->
+                            <!-- listing-item -->
+                            <div class="listing-item" style="width: 315px; padding: 30px 30px 0px 30px;"><a href="kategori-tidak-mampu.html">
+                                <article class="geodir-category-listing fl-wrap">
+                                    <div class="geodir-category-img">
+                                        <img src="images/kategori/mampu.jpg" alt="">
+                                    </div>
+                                </article>
+                                <div class="section-title" style="margin-top: 20px;">
+                                    <h4><b>SISWA TIDAK MAMPU</b></h4>
+                                </div></a>
+                            </div>
+                            <!-- listing-item end-->
+                            <!-- listing-item -->
+                            <div class="listing-item" style="width: 315px; padding: 30px 30px 0px 30px;"><a href="kategori-kecelakaan-sakit.html">
+                                <article class="geodir-category-listing fl-wrap">
+                                    <div class="geodir-category-img">
+                                        <img src="images/kategori/kecelakaan.jpg" alt="">
+                                    </div>
+                                </article>
+                                <div class="section-title" style="margin-top: 20px;">
+                                    <h4><b>KECELAKAAN / SAKIT</b></h4>
+                                </div></a>
+                            </div>
+                            <!-- listing-item end-->
+                            <!-- listing-item -->
+                            <div class="listing-item" style="width: 315px; padding: 30px 30px 0px 30px;"><a href="kategori-pembangunan.html">
+                                <article class="geodir-category-listing fl-wrap">
+                                    <div class="geodir-category-img">
+                                        <img src="images/kategori/bangun.jpg" alt="">
+                                    </div>
+                                </article>
+                                <div class="section-title" style="margin-top: 20px;">
+                                    <h4><b>PEMBANGUNAN</b></h4>
+                                </div></a>
+                            </div>
+                            <!-- listing-item end-->
+                            <!-- listing-item -->
+                            <div class="listing-item" style="width: 315px; padding: 0px 30px 0px 30px;"><a href="kategori-kebakaran.html">
+                                <article class="geodir-category-listing fl-wrap">
+                                    <div class="geodir-category-img">
+                                        <img src="images/kategori/bakar.jpg" alt="">
+                                    </div>
+                                </article>
+                                <div class="section-title" style="margin-top: 20px;">
+                                    <h4><b>KEBAKARAN</b></h4>
+                                </div></a>
+                            </div>
+                            <!-- listing-item end-->
+                            <!-- listing-item -->
+                            <div class="listing-item" style="width: 315px; padding: 0px 30px 0px 30px;"><a href="kategori-banjir.html">
+                                <article class="geodir-category-listing fl-wrap">
+                                    <div class="geodir-category-img">
+                                        <img src="images/kategori/banjir.jpg" alt="">
+                                    </div>
+                                </article>
+                                <div class="section-title" style="margin-top: 20px;">
+                                    <h4><b>BANJIR</b></h4>
+                                </div></a>
+                            </div>
+                            <!-- listing-item end-->
+                            <!-- listing-item -->
+                            <div class="listing-item" style="width: 315px; padding: 0px 30px 0px 30px;"><a href="kategori-tanah-longsor.html">
+                                <article class="geodir-category-listing fl-wrap">
+                                    <div class="geodir-category-img">
+                                        <img src="images/kategori/longsor.jpg" alt="">
+                                    </div>
+                                </article>
+                                <div class="section-title" style="margin-top: 20px;">
+                                    <h4><b>TANAH LONGSOR</b></h4>
+                                </div></a>
+                            </div>
+                            <!-- listing-item end-->
+                            <!-- listing-item -->
+                            <div class="listing-item" style="width: 315px; padding: 0px 30px 0px 30px;"><a href="#">
+                                <article class="geodir-category-listing fl-wrap">
+                                    <div class="geodir-category-img">
+                                        <img src="images/kategori/rip.jpg" alt="">
+                                    </div>
+                                </article>
+                                <div class="section-title" style="margin-top: 20px;">
+                                    <h4><b>DUKA CITA KELUARGA</b></h4>
+                                </div></a>
+                            </div>
+                            <!-- listing-item end-->
+                            <!-- listing-item -->
+                            <div class="listing-item" style="width: 315px; padding: 0px 30px 0px 30px;"><a href="#">
+                                <article class="geodir-category-listing fl-wrap">
+                                    <div class="geodir-category-img">
+                                        <img src="images/kategori/puting.jpg" alt="">
+                                    </div>
+                                </article>
+                                <div class="section-title" style="margin-top: 20px;">
+                                    <h4><b>PUTING BELIUNG</b></h4>
+                                </div></a>
+                            </div>
+                            <!-- listing-item end-->
+                            <!-- listing-item -->
+                            <div class="listing-item" style="width: 315px; padding: 0px 30px 0px 30px;"><a href="#">
+                                <article class="geodir-category-listing fl-wrap">
+                                    <div class="geodir-category-img">
+                                        <img src="images/kategori/coming.jpg" alt="">
+                                    </div>
+                                </article>
+                                <div class="section-title" style="margin-top: 20px;">
+                                    <h4><b>MASIH MIKIR</b></h4>
+                                </div></a>
+                            </div>
+                            <!-- listing-item end-->
+                        </div>
+                        
+                    </div>
+                    <!-- section end -->
+                    <!--section -->
+                                        <!-- section end -->
+                    <!--section -->
+                    
+                    </section>
+                    <!-- section end -->
+                    <!--section -->
+                                        <!-- section end -->
+                    <!--section -->                    <!-- section end -->
+                                        <!-- section end -->
+                    <div class="limit-box"></div>
+                </div>
--- user/application/modules/template/views/view_template_beranda.php
+++ user/application/modules/template/views/view_template_beranda.php
@@ -74,15 +74,15 @@ $menu = $uri_segments[3];
                     <button class="header-search-button" onclick="window.location.href='#'"><i class="fa fa-search"></i></button>
                 </div>
                 <div class="show-search-button"><i class="fa fa-search"></i> <span>Cari</span></div>
-                <a href="<?php echo base_url('#'); ?>" class="add-list">Galang Dana <span><i class="fa fa-plus"></i></span></a>
+                <a href="<?php echo base_url('galang_dana'); ?>" class="add-list">Galang Dana <span><i class="fa fa-plus"></i></span></a>
                 <div class="header-user-menu">
                     <div class="header-user-name">
                         <span><img src="images/avatar/4.jpg" alt=""></span>
                         Abdul Hasan
                     </div>
                     <ul>
-                        <li><a href="<?php echo base_url('#'); ?>">Dashboard</a></li>
-                        <li><a href="<?php echo base_url('#'); ?>">Log Out</a></li>
+                        <li><a href="<?php echo base_url('donasi_saya'); ?>"> Dashboard</a></li>
+                        <li><a href="<?php echo base_url('login'); ?>">Log Out</a></li>
                     </ul>
                 </div>
                 <!-- nav-button-wrap--> 
@@ -103,14 +103,14 @@ $menu = $uri_segments[3];
                                 <a href="#">KATEGORI <i class="fa fa-caret-down"></i></a>
                                 <!--second level -->
                                 <ul>
-                                    <li><a href="<?php echo base_url('#'); ?>">Siswa Putus Sekolah</a></li>
-                                    <li><a href="<?php echo base_url('#'); ?>">Siswa Tidak Mampu</a></li>
-                                    <li><a href="<?php echo base_url('#'); ?>">Kecelakaan / Sakit</a></li>
-                                    <li><a href="<?php echo base_url('#'); ?>">Pembangunan</a></li>
-                                    <li><a href="<?php echo base_url('#'); ?>">Kebakaran</a></li>
-                                    <li><a href="<?php echo base_url('#'); ?>">Banjir</a></li>
-                                    <li><a href="<?php echo base_url('#'); ?>">Tanah Longsor</a></li>
-                                    <li><a href="<?php echo base_url('#'); ?>">Lainnya ></a></li>
+                                    <li><a href="<?php echo base_url('kategori_siswa_putus_sekolah'); ?>">Siswa Putus Sekolah</a></li>
+                                    <li><a href="<?php echo base_url('kategori_tidak_mampu'); ?>">Siswa Tidak Mampu</a></li>
+                                    <li><a href="<?php echo base_url('kategori_kecelakaan'); ?>">Kecelakaan / Sakit</a></li>
+                                    <li><a href="<?php echo base_url('kategori_pembangunan'); ?>">Pembangunan</a></li>
+                                    <li><a href="<?php echo base_url('kategori_kebakaran'); ?>">Kebakaran</a></li>
+                                    <li><a href="<?php echo base_url('kategori_banjir'); ?>">Banjir</a></li>
+                                    <li><a href="<?php echo base_url('kategori_tanah_longsor'); ?>">Tanah Longsor</a></li>
+                                    <li><a href="<?php echo base_url('Kategori_lainnya'); ?>">Lainnya ></a></li>
                                 </ul>
                                 <!--second level end-->
                             </li>
@@ -204,10 +204,10 @@ $menu = $uri_segments[3];
                         <div class="footer-contacts-widget fl-wrap">
                             <p>Temukan jawabanmu disini</p>
                             <ul  class="footer-contacts fl-wrap">
-                                <li><a href="user-syarat-ketentuan.html">Syarat dan Ketentuan</a></li>
-                                <li><a href="user-tentang-kami.html">Tentang Kami</a></li>
+                                <li><a href="<?php echo base_url('syarat_ketentuan'); ?>">Syarat dan Ketentuan</a></li>
+                                <li><a href="<?php echo base_url('tentang_kami'); ?>">Tentang Kami</a></li>
                                 <li><a href="<?php echo base_url('hubungi_kami'); ?>">Hubungi Kami</a></li>
-                                <li><a href="pusat-bantuan-user.html">FAQ</a></li>
+                                <li><a href="<?php echo base_url('faq'); ?>">FAQ</a></li>
                             </ul>
                         </div>
                     </div>
