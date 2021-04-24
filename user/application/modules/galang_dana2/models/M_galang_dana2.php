524801f3934e72bed6bf95a84cd09f0b090cd387
boyabdulhasan <boyabdulhasan@gmail.com>
2021-04-24 13:42:20 +0700

    update

--- user/application/modules/galang_dana2/models/M_galang_dana2.php
+++ user/application/modules/galang_dana2/models/M_galang_dana2.php
@@ -10,9 +10,6 @@ class M_galang_dana2 extends CI_Model {
 		$this->db->from('galang_dana2');
 		$query = $this->db->get();
 
-
-
-
 		return $query->result();
 	}
 
@@ -23,7 +20,7 @@ class M_galang_dana2 extends CI_Model {
 
 		$this->db->select('*');
 		$this->db->from('galang_dana2');
-		$this->db->where('id_galangdana2',$idnya);
+		$this->db->where('id_galang_dana2',$idnya);
 		$query = $this->db->get();
 
 
@@ -33,56 +30,21 @@ class M_galang_dana2 extends CI_Model {
 
 	
 
-
 	function tambah()
 	{
 
 
-		$nama_pembuka 				= $this->input->post('nama_pembuka');
-		$status_pembuka 			= $this->input->post('status_pembuka');
-		$jenjang_pembuka 			= $this->input->post('jenjang_pembuka');
-		$sekolah_pembuka 			= $this->input->post('sekolah_pembuka');
-		$kelas_pembuka				= $this->input->post('kelas_pembuka');
-		$telepon_pembuka			= $this->input->post('telepon_pembuka');
-		$wa_pembuka 				= $this->input->post('wa_pembuka');
-		$identitas_pembuka 			= $this->input->post('identitas_pembuka');
-		$provinsi		 			= $this->input->post('provinsi');
-		$kota 						= $this->input->post('kota');
-		$kecamatan 					= $this->input->post('kecamatan');
-		$kelurahan	 				= $this->input->post('kelurahan');
-		$desa	 					= $this->input->post('desa');
-		$kode_pos 					= $this->input->post('kode_pos');
-		$alamat_pembuka 			= $this->input->post('alamat_pembuka ');
-		$nama_penerima 				= $this->input->post('nama_penerima');
-		$alamat_penerima			= $this->input->post('alamat_penerima');
-		$jenjang_penerima 			= $this->input->post('jenjang_penerima');
-		$sekolah_penerima 			= $this->input->post('sekolah_penerima');
-		$kelas_penerima 			= $this->input->post('kelas_penerima');
-		$telepon_penerima 			= $this->input->post('telepon_penerima');
-		$wa_penerima 				= $this->input->post('wa_penerima');
-		$email_penerima 			= $this->input->post('email_penerima');
-		$identitas_penerima 		= $this->input->post('identitas_penerima');
-		$judul_donasi		 		= $this->input->post('judul_donasi');
-		$deskripsi_peristiwa		= $this->input->post('deskripsi_peristiwa');
-		$dokumentasi_perisiwa		= $this->input->post('dokumentasi_peristiwa');
-		$tgl_awal 					= $this->input->post('tgl_awal');
-		$tgl_akhir 					= $this->input->post('tgl_akhir');
-		$jumlah_donasi 				= $this->input->post('jumlah_donasi');
-		$provinsi_penerima 			= $this->input->post('provinsi_penerima');
-		$kota_penerima 				= $this->input->post('kota_penerima');
-		$kecamatan_penerima 		= $this->input->post('kecamatan_penerima');
-		$kelurahan_penerima 		= $this->input->post('kelurahan_penerima');
-		$kode_pos_penerima 			= $this->input->post('kode_pos_penerima');
-		$tgl_dibuat					= $this->input->post('tgl_dibuat');
+		$nama_pembuka		= $this->input->post('nama_pembuka');
+		$no_rekening		= $this->input->post('no_rekening');
+		$telepon_pembuka    = $this->input->post('telepon_pembuka');
+		$wa_pembuka			= $this->input->post('wa_pembuka');
+		$instagram	    	= $this->input->post('instagram');
+		$alamat_pembuka 	= $this->input->post('alamat_pembuka');
 
-		$insert_awal = date( 'Y-m-d', strtotime($tgl_awal));
-		$insert_akhir = date( 'Y-m-d', strtotime($tgl_akhir));
-
-
-
+		
 		$this->load->library('upload');
 		$nmfile = "file_".time();
-		$config['upload_path']		= 'assets/identitas_pembuka/pembuka/';
+		$config['upload_path']		= 'images/galangdana/';
 		$config['allowed_types']	= 'gif|jpg|png|jpeg';
 		$config['max_size']			= 5120;
 		$config['max_width']		= 4300;
@@ -91,48 +53,19 @@ class M_galang_dana2 extends CI_Model {
 		
 		$this->upload->initialize($config);
 		
-		if($_FILES['gambar']['name'])
+		if($_FILES['foto']['name'])
         {
-            if ($this->upload->do_upload('gambar'))
+            if ($this->upload->do_upload('foto'))
             {
 				$gbr = $this->upload->data();
 				$data = array(
-					'nama_pembuka'				=> $nama_pembuka,
-					'status_pembuka'			=> $status_pembuka,
-					'jenjang_pembuka'			=> $jenjang_pembuka,
-					'sekolah_pembuka'			=> $sekolah_pembuka,
-					'kelas_pembuka'				=> $kelas_pembuka,
-					'telepon_pembuka'			=> $telepon_pembuka,
-					'wa_pembuka'				=> $wa_pembuka,
-					'identitas_pembuka' 		=> $gbr['file_name'],
-					'provinsi'					=> $provinsi,
-					'kota'						=> $kota,
-					'kelurahan'					=> $kelurahan,
-					'kecamatan'					=> $kecamatan,
-					'desa'						=> $desa,
-					'kode_pos'					=> $kode_pos,
-					'alamat_pembuka'			=> $alamat_pembuka,
-					'nama_penerima'				=> $nama_penerima,
-					'alamat_penerima'			=> $alamat_penerima,
-					'status_penerima'			=> $status_penerima,
-					'jenjang_penerima'			=> $jenjang_penerima,
-					'sekolah_penerima'			=> $sekolah_penerima,
-					'kelas_penerima'			=> $kelas_penerima,
-					'telepon_penerima'			=> $telepon_penerima,
-					'wa_penerima'				=> $wa_penerima,
-					'email_penerima'			=> $email_penerima,
-					'identitas_penerima' 		=> $identitas_penerima,
-					'judul_donasi'		 		=> $judul_donasi,
-					'deskripsi_peristiwa'		=> $deskripsi_peristiwa,
-					'dokumentasi_peristiwa' 	=> $dokumentasi_peristiwa,
-					'tgl_awal'					=> $tgl_awal,
-					'tgl_akhir'					=> $tgl_akhir,
-					'jumlah_donasi'				=> $jumlah_donasi,
-					'provinsi_penerima'			=> $provinsi_penerima,
-					'kabupaten_penerima'		=> $kabupaten_penerima,
-					'kecamatan_penerima'		=> $kecamatan_penerima,
-					'kelurahan_penerima'		=> $kelurahan_penerima,
-					
+					'nama_pembuka'			=> $nama_pembuka,
+					'no_rekening'			=> $no_rekening,
+					'telepon_pembuka'      	=> $telepon_pembuka,
+					'wa_pembuka'    		=> $wa_pembuka,
+					'instagram'     		=> $instagram,
+					'alamat_pembuka'       	=> $alamat_pembuka,
+					'identitas_pembuka' 	=> $gbr['file_name'],
 					
 					
 				);
@@ -143,39 +76,13 @@ class M_galang_dana2 extends CI_Model {
 		}
 		else{
 				$data = array(
-					'nama_pembuka'				=> $nama_pembuka,
-					'status_pembuka'			=> $status_pembuka,
-					'jenjang_pembuka'			=> $jenjang_pembuka,
-					'sekolah_pembuka'			=> $sekolah_pembuka,
-					'kelas_pembuka'				=> $kelas_pembuka,
-					'telepon_pembuka'			=> $telepon_pembuka,
-					'wa_pembuka'				=> $wa_pembuka,
-					'identitas_pembuka' 		=> 'kosong1.png',
-					'provinsi'					=> $provinsi,
-					'kota'						=> $kota,
-					'kelurahan'					=> $kelurahan,
-					'kecamatan'					=> $kecamatan,
-					'desa'						=> $desa,
-					'kode_pos'					=> $kode_pos,
-					'alamat_pembuka'			=> $alamat_pembuka,
-					'nama_penerima'				=> $nama_penerima,
-					'alamat_penerima'			=> $alamat_penerima,
-					'jenjang_penerima'			=> $jenjang_penerima,
-					'sekolah_penerima'			=> $sekolah_penerima,
-					'kelas_penerima'			=> $kelas_penerima,
-					'telepon_penerima'			=> $telepon_penerima,
-					'wa_penerima'				=> $wa_penerima,
-					'email_penerima'			=> $email_penerima,
-					'identitas_penerima' 		=> $identitas_penerima,
-					'judul_donasi' 				=> $judul_donasi,
-					'deskripsi_peristiwa'		=> $deskripsi_peristiwa,
-					'dokumentasi_peristiwa' 	=> $deskripsi_peristiwa,
-					'tgl_awal'					=> $tgl_awal,
-					'tgl_akhir'					=> $tgl_akhir,
-					'jumlah_donasi'				=> $jumlah_donasi,
-					'provinsi_penerima'			=> $provinsi_penerima,
-					'kota_penerima'				=> $kota_penerima,
-					'kecamatan_penerima'		=> $kecamatan_penerima,
+					'nama_pembuka'			=> $nama_pembuka,
+					'no_rekening'			=> $no_rekening,
+					'telepon_pembuka'      	=> $telepon_pembuka,
+					'wa_pembuka'    		=> $wa_pembuka,
+					'instagram'     		=> $instagram,
+					'alamat_pembuka'       	=> $alamat_pembuka,
+					'identitas_pembuka' 	=> 'kosong.jpeg',
 					
 				);
 				$this->db->insert('galang_dana2', $data);
@@ -187,52 +94,18 @@ class M_galang_dana2 extends CI_Model {
 	{
 		$id = $this->input->post('id');
 
-		$nama_pembuka 				= $this->input->post('nama_pembuka');
-		$status_pembuka 			= $this->input->post('status_pembuka');
-		$jenjang_pembuka 			= $this->input->post('jenjang_pembuka');
-		$sekolah_pembuka 			= $this->input->post('sekolah_pembuka');
-		$kelas_pembuka				= $this->input->post('kelas_pembuka');
-		$telepon_pembuka			= $this->input->post('telepon_pembuka');
-		$wa_pembuka 				= $this->input->post('wa_pembuka');
-		$email_pembuka 				= $this->input->post('email_pembuka');
-		$identitas_pembuka	        = $this->input->post('identitas_pembuka');
-		$provinsi					= $this->input->post('provinsi');
-		$kota		 				= $this->input->post('kota');
-		$kelurahan		 			= $this->input->post('kelurahan');
-		$kecamatan	 				= $this->input->post('kecamatan');
-		$desa	 					= $this->input->post('desa');
-		$kode_pos		 			= $this->input->post('kode_pos');
-		$alamat_pembuka 			= $this->input->post('alamat_pembuka');
-		$nama_penerima 				= $this->input->post('nama_penerima');
-		$status_penerima 			= $this->input->post('status_penerima');
-		$jenjang_penerima 			= $this->input->post('jenjang_penerima');
-		$sekolah_penerima 			= $this->input->post('sekolah_penerima');
-		$kelas_penerima 			= $this->input->post('kelas_penerima');
-		$telepon_penerima 			= $this->input->post('telepon_penerima');
-		$wa_penerima 				= $this->input->post('wa_penerima ');
-		$email_penerima 			= $this->input->post('email_penerima');
-		$identitas_penerima 		= $this->input->post('identitas_penerima');
-		$deskripsi_peristiwa		= $this->input->post('deskripsi_peristiwa');
-		$dokumentasi_peristiwa		= $this->input->post('dokumentasi_peristiwa');
-		$tgl_awal 					= $this->input->post('tgl_awal');
-		$tgl_akhir 					= $this->input->post('tgl_akhir');
-		$jml_donasi 				= $this->input->post('jml_donasi');
-		$provinsi_penerima 			= $this->input->post('provinsi_penerima');
-		$kabupaten_penerima 		= $this->input->post('kabupaten_penerima');
-		$kecamatan_penerima 		= $this->input->post('kecamatan_penerima');
-		$desa_penerima 				= $this->input->post('desa_penerima');
-		$kode_pos_penerima 			= $this->input->post('kode_pos_penerima');
-		$alamat_penerima 			= $this->input->post('alamat_penerima ');
-		$tgl_dibuat					= $this->input->post('tgl_dibuat');
-
-		$insert_awal = date( 'Y-m-d', strtotime($tgl_awal));
-		$insert_akhir = date( 'Y-m-d', strtotime($tgl_akhir));
-
+		$nominal_donasi		= $this->input->post('nominal_donasi');
+		$nama_lengkap		= $this->input->post('nama_lengkap');
+		$tgl_pembayaran     = $this->input->post('tgl_pembayaran');
+		$nama_rekening		= $this->input->post('nama_rekening');
+		$bukti_pembayaran   = $this->input->post('bukti_pembayaran ');
+		$pesan_kesan 		= $this->input->post('pesan_kesan');
+	
 
 
 		$this->load->library('upload');
 		$nmfile = "file_".time();
-		$config['upload_path']		= 'assets/identitas/pembuka';
+		$config['upload_path']		= 'images/galangdana/';
 		$config['allowed_types']	= 'gif|jpg|png|jpeg';
 		$config['max_size']			= 5120;
 		$config['max_width']		= 4300;
@@ -241,85 +114,39 @@ class M_galang_dana2 extends CI_Model {
 		
 		$this->upload->initialize($config);
 		
-		if($_FILES['gambar']['name'])
+		if($_FILES['foto']['name'])
         {
-            if ($this->upload->do_upload('gambar'))
+            if ($this->upload->do_upload('foto'))
             {
 				$gbr = $this->upload->data();
 				$data = array(
-					'nama_pembuka'				=> $nama_pembuka,
-					'status_pembuka'			=> $status_pembuka,
-					'jenjang_pembuka'			=> $jenjang_pembuka,
-					'sekolah_pembuka'			=> $sekolah_pembuka,
-					'kelas_pembuka'				=> $kelas_pembuka,
-					'telepon_pembuka'			=> $telepon_pembuka,
-					'wa_pembuka'				=> $wa_pembuka,
-					'email_pembuka'				=> $email_pembuka,
-					'identitas'			 		=> $gbr['file_name'],
-					'provinces'					=> $provinces,
-					'regencies'					=> $regencies,
-					'districts'					=> $districts,
-					'villages'					=> $villages,
-					'alamat_lengkap_pembuka'	=> $alamat_lengkap_pembuka,
-					'nama_penerima'				=> $nama_penerima,
-					'status_penerima'			=> $status_penerima,
-					'jenjang_penerima'			=> $jenjang_penerima,
-					'sekolah_penerima'			=> $sekolah_penerima,
-					'kelas_penerima'			=> $kelas_penerima,
-					'telepon_penerima'			=> $telepon_penerima,
-					'wa_penerima'				=> $wa_penerima,
-					'email_penerima'			=> $email_penerima,
-					'identitas_penerima' 		=> $identitas_penerima,
-					'deskripsi_peristiwa'		=> $deskripsi_peristiwa,
-					'dokumentasi_peristiwa' 	=> $dokumentasi_peristiwa,
-					'tgl_awal'					=> $tgl_awal,
-					'tgl_akhir'					=> $tgl_akhir,
-					'jml_donasi'				=> $jml_donasi,
-					'provinsi_penerima'			=> $provinsi_penerima,
-					'kabupaten_penerima'		=> $kabupaten_penerima,
-					'kecamatan_penerima'		=> $kecamatan_penerima,
-					'desa_penerima'				=> $desa_penerima,
-					'alamat_penerima'			=> $alamat_penerima,
+					'id_pembayaran'		=> $id_pembayaran,
+					'nominal_donasi'	=> $nominal_donasi,
+					'nama_lengkap'      => $nama_lengkap,
+					'tgl_pembayaran'    => $tgl_pembayaran,
+					'nama_rekening'     => $nama_rekening,
+					'no_rekening'       => $no_rekening,
+					'bukti_pembayaran' 	=> $bukti_pembayaran,
+					'pesan_kesan'       => $pesan_kesan,
+					'identitas_pembuka' 	=> $gbr['file_name'],
 				);
-				$this->db->where('id_galang_dana2',$id)->update('galang_dana2', $data);
+				$this->db->where('id_pembayaran',$id)->update('pembayaran_user', $data);
 				$this->session->set_flashdata('msg', 'suksesedit');
 			
 			}	 
 		}
 		else{
 				$data = array(
-					'nama_pembuka'				=> $nama_pembuka,
-					'status_pembuka'			=> $status_pembuka,
-					'jenjang_pembuka'			=> $jenjang_pembuka,
-					'sekolah_pembuka'			=> $sekolah_pembuka,
-					'kelas_pembuka'				=> $kelas_pembuka,
-					'telepon_pembuka'			=> $telepon_pembuka,
-					'wa_pembuka'				=> $wa_pembuka,
-					'email_pembuka'				=> $email_pembuka,
-					'provinces'					=> $provinces,
-					'regencies'					=> $regencies,
-					'districts'					=> $districts,
-					'villages'					=> $villages,
-					'alamat_lengkap_pembuka'	=> $alamat_lengkap_pembuka,
-					'nama_penerima'				=> $nama_penerima,
-					'status_penerima'			=> $status_penerima,
-					'jenjang_penerima'			=> $jenjang_penerima,
-					'sekolah_penerima'			=> $sekolah_penerima,
-					'kelas_penerima'			=> $kelas_penerima,
-					'telepon_penerima'			=> $telepon_penerima,
-					'wa_penerima'				=> $wa_penerima,
-					'email_penerima'			=> $email_penerima,
-					'deskripsi_peristiwa'		=> $deskripsi_peristiwa,
-					'tgl_awal'					=> $tgl_awal,
-					'tgl_akhir'					=> $tgl_akhir,
-					'jml_donasi'				=> $jml_donasi,
-					'provinsi_penerima'			=> $provinsi_penerima,
-					'kabupaten_penerima'		=> $kabupaten_penerima,
-					'kecamatan_penerima'		=> $kecamatan_penerima,
-					'desa_penerima'				=> $desa_penerima,
-					'alamat_penerima'			=> $alamat_penerima,
+					'id_pembayaran'		=> $id_pembayaran,
+					'nominal_donasi'	=> $nominal_donasi,
+					'nama_lengkap'      => $nama_lengkap,
+					'tgl_pembayaran'    => $tgl_pembayaran,
+					'nama_rekening'     => $nama_rekening,
+					'no_rekening'       => $no_rekening,
+					'bukti_pembayaran' 	=> $bukti_pembayaran,
+					'pesan_kesan'       => $pesan_kesan,
 				);
-				$this->db->where('id_galangdana2',$id)->update('galang_dana2', $data);
+				$this->db->where('id_pembayaran',$id)->update('pembayaran_user', $data);
 				$this->session->set_flashdata('msg', 'suksesedit');
 			}
 	}
@@ -327,7 +154,7 @@ class M_galang_dana2 extends CI_Model {
 	function hapus()
 	{
 		$id = $this->input->post('id');
-		$this->db->where('id_galang_dana2', $id)->delete('galang_dana2');
+		$this->db->where('id_galang_dana', $id)->delete('galang_dana2');
 		$this->session->set_flashdata('msg', 'sukseshapus');
 	}
 
--- user/application/modules/galang_dana2/views/V_galang_dana2.php
+++ user/application/modules/galang_dana2/views/V_galang_dana2.php
@@ -6,115 +6,55 @@
                                         <i class="pe-7s-medal icon-gradient bg-tempting-azure"></i>
                                     </div>
                                     <div>Data Galang Dana
-                                        <div class="page-title-subheading">Data Peralatan Dan Mesin , silahkan gunakan menu di samping untuk menambahkan </div>
+                                        <div class="page-title-subheading">Halaman galang dana</div>
                                     </div>
                                 </div>
                                 <div class="page-title-actions">
-                                  
-                                    <div class="d-inline-block dropdown">
-                                        <button type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="btn-shadow dropdown-toggle btn btn-info">
-                                            <span class="btn-icon-wrapper pr-2 opacity-7">
-                                                <i class="fa fa-business-time fa-w-20"></i>
-                                            </span>
-                                            Menu
-                                        </button>
-                                        <div tabindex="-1" role="menu" aria-hidden="true" class="dropdown-menu dropdown-menu-right">
-                                            <ul class="nav flex-column">
-                                                <li class="nav-item">
-                                                    <a class="nav-link" href="galang_dana2/tambahview">
-                                                        <i class="nav-link-icon lnr-inbox"></i>
-                                                        <span> Tambah Data</span>
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </div>
-                                    </div>
+
+                                  <a href="<?php echo base_url('galang_dana2/tambahview'); ?>" class="mb-2 mr-2 btn btn-shadow btn-success">Tambah Data</a>
+
                                 </div>    
+                              </div>
+                        </div>
+
+                        <?php tampilnotif()?>
+                            <div class="list-main-wrap fl-wrap card-listing" style="padding: 10px 20px 20px 20px;">
+                            <?php $no=1; foreach ($tampil AS $rowP ) { ?>
+                                <div class="listing-item" style="width: 320px;">
+                                    <article class="geodir-category-listing fl-wrap">
+                                        <div class="geodir-category-img">
+                                            <img src="<?php echo base_url() ?>images/galangdana/<?php echo $rowP->identitas_pembuka;?>">
+                                            <div class="overlay"></div>
+                                        </div>
+                                        <div class="geodir-category-content fl-wrap" style="height : 280px;">
+                                          <div class="listing-avatar"><a href="author-single.html"><img src="images/avatar/5.jpg" alt=""></a>
+                                              <span class="avatar-tooltip">Posting oleh  <strong>Dhinda</strong></span>
+                                          </div>
+                                          <h3><a href="user-detail-donasi.html"><?php echo $rowP->nama_pembuka;?></a></h3>
+                                          <p><b>120</b> <i> hari tersisa</i>  </p>
+                                          <div class="distance-title"> 
+                                              <b>Rp 4.580,000</b> Terkumpul   <span style="padding-left: 60px;">1</span> %
+                                          </div>
+                                              <div class="distance-radius-wrap fl-wrap">
+                                                  <input class="distance-radius rangeslider--horizontal" type="range" min="1" max="100" step="1" value="1" data-title="Radius around selected destination" style="position: absolute; width: 1px; height: 1px; overflow: hidden; opacity: 0;">
+                                                  <div class="rangeslider rangeslider--horizontal" id="js-rangeslider-0">
+                                                  </div>
+                                              </div>
+                                          <div class="geodir-category-options fl-wrap">
+                                              <div class="geodir-category-location"><i>Target Rp 8,000,000</i></div>
+                                          </div>
+                                        </div>
+                                    </article>
+                                </div>
+                            <?php $no++;} ?>
                             </div>
-                        </div> 
-
-            <?php tampilnotif()?>
-            <div class="row">
-                <?php $no=1;
-                foreach ($tampil AS $rowP ) { ?>
-                    <div class="col-sm-12 col-md-4">
-                        <div class="card-hover profile-responsive card-border border-success mb-3 card">
-                        <div class="dropdown-menu-header">
-                            <div class="wrapper rounded"></div>
-                            <img src="assets/images/Peristiwa/2.jpg" style="width: 305px; height:210px;">   
+                        <div class="text-center">
+                            <button class="btn-shadow btn-wide btn-pill btn btn-primary">
+                                Lihat Lainnya
+                            </button>
                         </div>
-                        <div class="p-0 card-body">
-                            
-
-                            <div class="tab-content">
-                                <div class="tab-pane active show" id="tab-2-eg1">
-                                    <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">
-                                            <div class="h5 modal-title">
-                                                <div class="text-right">
-                                                    <h6>
-                                                        10/10/2020
-                                                    </h6>
-                                                </div>
-                                                <h5 class="menu-header-title">
-                                                    <div ><?php echo $rowP->judul_donasi;?></div>
-                                                
-
-                                                </h5>
-                                                <h6>
-                                            <div>Diposting oleh @Sandrajamet123</div>
-                                                </h6>
-                                            </div>
-                                        </li>
-                                        <li class="list-group-item">
-                                            Rp 0 Terkumpul dari Rp 15.000.000    
-                                            <div class="progress-bar-xs progress-bar-animated-alt progress">
-                                            <div class="progress-bar bg-info" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style="width: 65%;">
-                                            </div>
-                                            </div>
-                                            <div> <br> </div>
-                                            <div> Sisa Waktu : 0 Hari</div>
-                                            <div> 465 Orang telah Berdonasi</div>
-                                        </li>
-                                        <div class="card-body">
-                                        <div class="br-wrapper br-theme-css-stars"><select id="css-stars" style="display: none;">
-                                            <option value="1">1</option>
-                                            <option value="2">2</option>
-                                            <option value="3">3</option>
-                                            <option value="4">4</option>
-                                            <option value="5">5</option>
-                                        </select><div class="br-widget"><a href="#" data-rating-value="1" data-rating-text="1" class="br-selected br-current"></a><a href="#" data-rating-value="2" data-rating-text="2" class=""></a><a href="#" data-rating-value="3" data-rating-text="3"></a><a href="#" data-rating-value="4" data-rating-text="4"></a><a href="#" data-rating-value="5" data-rating-text="5"></a><div class="br-current-rating">1</div></div></div>
-                                    </div>
-
-                                    </ul>
-                                </div>
-                                <div class="tab-pane" id="tab-2-eg2">
-                                    <ul class="list-group list-group-flush"><a href="javascript:void(0);" class="disabled list-group-item">Cras justo odio</a><a href="javascript:void(0);" class="list-group-item">Dapibus ac facilisis in</a><a href="javascript:void(0);" class="list-group-item">Morbi leo risus</a><a href="javascript:void(0);" class="list-group-item">Porta ac consectetur ac</a><a href="javascript:void(0);" class="list-group-item">Vestibulum at eros</a>
-                                    </ul>
-                                </div>
-                                <div class="tab-pane" id="tab-2-eg3">
-                                    <div class="p-3">
-                                        <p>With supporting text below as a natural lead-in to additional content.</p>
-                                        <p class="mb-0">Lorem Ipsum has been the industry's standard dummy text ever since
-                                            the 1500s, when an unknown printer took a galley of type and scrambled.</p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            
-
-                        </div>
-                        <div class="text-center d-block card-footer">
-                            <a href="<?php echo base_url('detail_donasi'); ?>" class="btn-shadow-primary btn btn-primary btn-lg">Detail</a>
-                            <button type="button" class="btn-shadow-primary btn btn-primary btn-lg" data-toggle="modal" data-target=".bd-example-modal-sm">Bagikan</button>
-                        </div>
-                        </div>
-     
                     </div>
-                <?php $no++;} ?>
-            </div>
 
-            
-                
-</div>
-
+                  
+                   
+                          
