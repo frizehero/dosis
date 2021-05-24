e3a477e2fb27d70002f595c8ae4760ad53e7347c
tiaranh_ <tiarahasanah5055@gmail.com>
2021-05-24 13:45:49 +0700

    upd

--- admin/application/modules/detail_donasi/models/M_detail_donasi.php
+++ admin/application/modules/detail_donasi/models/M_detail_donasi.php
@@ -7,7 +7,7 @@ class M_detail_donasi extends CI_Model {
 	{
 
 		$this->db->select('*');
-		$this->db->from('galang_dana2');
+		$this->db->from('detail_donasi');
 		$query = $this->db->get();
 
 
@@ -21,8 +21,8 @@ class M_detail_donasi extends CI_Model {
 		$idnya=decrypt_url($id);
 
 		$this->db->select('*');
-		$this->db->from('galang_dana2');
-		$this->db->where('id_galangdana2',$idnya);
+		$this->db->from('detail_donasi');
+		$this->db->where('id_detail_donasi',$idnya);
 		$query = $this->db->get();
 
 
@@ -32,41 +32,9 @@ class M_detail_donasi extends CI_Model {
 
 	function tambah()
 	{
-		$nama_pembuka 				= $this->input->post('nama_pembuka');
-		$status_pembuka 			= $this->input->post('status_pembuka');
-		$jenjang_pembuka 			= $this->input->post('jenjang_pembuka');
-		$sekolah_pembuka 			= $this->input->post('sekolah_pembuka');
-		$kelas_pembuka				= $this->input->post('kelas_pembuka');
-		$telepon_pembuka			= $this->input->post('telepon_pembuka');
-		$wa_pembuka 				= $this->input->post('wa_pembuka');
-		$identitas_pembuka 			= $this->input->post('identitas_pembuka');
-		$provinsi		 			= $this->input->post('provinsi');
-		$kota 						= $this->input->post('kota');
-		$kecamatan 					= $this->input->post('kecamatan');
-		$kelurahan	 				= $this->input->post('kelurahan');
-		$desa	 					= $this->input->post('desa');
-		$kode_pos 					= $this->input->post('kode_pos');
-		$alamat_pembuka 			= $this->input->post('alamat_pembuka ');
-		$nama_penerima 				= $this->input->post('nama_penerima');
-		$alamat_penerima			= $this->input->post('alamat_penerima');
-		$jenjang_penerima 			= $this->input->post('jenjang_penerima');
-		$sekolah_penerima 			= $this->input->post('sekolah_penerima');
-		$kelas_penerima 			= $this->input->post('kelas_penerima');
-		$telepon_penerima 			= $this->input->post('telepon_penerima');
-		$wa_penerima 				= $this->input->post('wa_penerima');
-		$email_penerima 			= $this->input->post('email_penerima');
-		$identitas_penerima 		= $this->input->post('identitas_penerima');
-		$judul_donasi		 		= $this->input->post('judul_donasi');
-		$deskripsi_peristiwa		= $this->input->post('deskripsi_peristiwa');
-		$dokumentasi_perisiwa		= $this->input->post('dokumentasi_peristiwa');
-		$tgl_awal 					= $this->input->post('tgl_awal');
-		$tgl_akhir 					= $this->input->post('tgl_akhir');
-		$jumlah_donasi 				= $this->input->post('jumlah_donasi');
-		$provinsi_penerima 			= $this->input->post('provinsi_penerima');
-		$kota_penerima 				= $this->input->post('kota_penerima');
-		$kecamatan_penerima 		= $this->input->post('kecamatan_penerima');
-		$kelurahan_penerima 		= $this->input->post('kelurahan_penerima');
-		$kode_pos_penerima 			= $this->input->post('kode_pos_penerima');
+		$deskripsi_peristiwa 		= $this->input->post('deskripsi_peristiwa');
+		$dokumentasi_peristiwa2 	= $this->input->post('dokumentasi_peristiwa2');
+		$dokumentasi_peristiwa3 	= $this->input->post('dokumentasi_peristiwa3');
 		$tgl_dibuat					= $this->input->post('tgl_dibuat');
 
 		$insert_awal = date( 'Y-m-d', strtotime($tgl_awal));
@@ -76,7 +44,7 @@ class M_detail_donasi extends CI_Model {
 
 		$this->load->library('upload');
 		$nmfile = "file_".time();
-		$config['upload_path']		= 'assets/identitas_pembuka/pembuka/';
+		$config['upload_path']		= 'assets/images/galang_dana/';
 		$config['allowed_types']	= 'gif|jpg|png|jpeg';
 		$config['max_size']			= 5120;
 		$config['max_width']		= 4300;
@@ -85,47 +53,15 @@ class M_detail_donasi extends CI_Model {
 		
 		$this->upload->initialize($config);
 		
-		if($_FILES['gambar']['name'])
+		if($_FILES['foto']['name'])
         {
-            if ($this->upload->do_upload('gambar'))
+            if ($this->upload->do_upload('foto'))
             {
 				$gbr = $this->upload->data();
 				$data = array(
-					'nama_pembuka'				=> $nama_pembuka,
-					'status_pembuka'			=> $status_pembuka,
-					'jenjang_pembuka'			=> $jenjang_pembuka,
-					'sekolah_pembuka'			=> $sekolah_pembuka,
-					'kelas_pembuka'				=> $kelas_pembuka,
-					'telepon_pembuka'			=> $telepon_pembuka,
-					'wa_pembuka'				=> $wa_pembuka,
-					'identitas_pembuka' 		=> $gbr['file_name'],
-					'provinsi'					=> $provinsi,
-					'kota'						=> $kota,
-					'kelurahan'					=> $kelurahan,
-					'kecamatan'					=> $kecamatan,
-					'desa'						=> $desa,
-					'kode_pos'					=> $kode_pos,
-					'alamat_pembuka'			=> $alamat_pembuka,
-					'nama_penerima'				=> $nama_penerima,
-					'alamat_penerima'			=> $alamat_penerima,
-					'status_penerima'			=> $status_penerima,
-					'jenjang_penerima'			=> $jenjang_penerima,
-					'sekolah_penerima'			=> $sekolah_penerima,
-					'kelas_penerima'			=> $kelas_penerima,
-					'telepon_penerima'			=> $telepon_penerima,
-					'wa_penerima'				=> $wa_penerima,
-					'email_penerima'			=> $email_penerima,
-					'identitas_penerima' 		=> $identitas_penerima,
-					'judul_donasi'		 		=> $judul_donasi,
 					'deskripsi_peristiwa'		=> $deskripsi_peristiwa,
-					'dokumentasi_peristiwa' 	=> $dokumentasi_peristiwa,
-					'tgl_awal'					=> $tgl_awal,
-					'tgl_akhir'					=> $tgl_akhir,
-					'jumlah_donasi'				=> $jumlah_donasi,
-					'provinsi_penerima'			=> $provinsi_penerima,
-					'kabupaten_penerima'		=> $kabupaten_penerima,
-					'kecamatan_penerima'		=> $kecamatan_penerima,
-					'kelurahan_penerima'		=> $kelurahan_penerima,
+					'dokumentasi_peristiwa2'	=> $gbr['file_name'],
+					'dokumentasi_peristiwa3'	=> $dokumentasi_peristiwa3,
 					
 					
 					
@@ -137,39 +73,9 @@ class M_detail_donasi extends CI_Model {
 		}
 		else{
 				$data = array(
-					'nama_pembuka'				=> $nama_pembuka,
-					'status_pembuka'			=> $status_pembuka,
-					'jenjang_pembuka'			=> $jenjang_pembuka,
-					'sekolah_pembuka'			=> $sekolah_pembuka,
-					'kelas_pembuka'				=> $kelas_pembuka,
-					'telepon_pembuka'			=> $telepon_pembuka,
-					'wa_pembuka'				=> $wa_pembuka,
-					'identitas_pembuka' 		=> 'kosong1.png',
-					'provinsi'					=> $provinsi,
-					'kota'						=> $kota,
-					'kelurahan'					=> $kelurahan,
-					'kecamatan'					=> $kecamatan,
-					'desa'						=> $desa,
-					'kode_pos'					=> $kode_pos,
-					'alamat_pembuka'			=> $alamat_pembuka,
-					'nama_penerima'				=> $nama_penerima,
-					'alamat_penerima'			=> $alamat_penerima,
-					'jenjang_penerima'			=> $jenjang_penerima,
-					'sekolah_penerima'			=> $sekolah_penerima,
-					'kelas_penerima'			=> $kelas_penerima,
-					'telepon_penerima'			=> $telepon_penerima,
-					'wa_penerima'				=> $wa_penerima,
-					'email_penerima'			=> $email_penerima,
-					'identitas_penerima' 		=> $identitas_penerima,
-					'judul_donasi' 				=> $judul_donasi,
 					'deskripsi_peristiwa'		=> $deskripsi_peristiwa,
-					'dokumentasi_peristiwa' 	=> $dokumentasi_peristiwa,
-					'tgl_awal'					=> $tgl_awal,
-					'tgl_akhir'					=> $tgl_akhir,
-					'jumlah_donasi'				=> $jumlah_donasi,
-					'provinsi_penerima'			=> $provinsi_penerima,
-					'kota_penerima'				=> $kota_penerima,
-					'kecamatan_penerima'		=> $kecamatan_penerima,
+					'dokumentasi_peristiwa2'	=> 'kosong1.png',
+					'dokumentasi_peristiwa3'	=> $dokumentasi_peristiwa3,
 					
 				);
 				$this->db->insert('detail_donasi', $data);
