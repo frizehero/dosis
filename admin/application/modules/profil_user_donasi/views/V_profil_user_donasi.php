06d817a6f7747b2b120256da97b914224321a2b0
indahnovita <79124897+indahnovita@users.noreply.github.com>
2021-06-04 17:03:11 +0700

    update

---
+++ admin/application/modules/profil_user_donasi/views/V_profil_user_donasi.php
@@ -0,0 +1,136 @@
+ <div class="app-main__inner">
+                    <div class="tabs-animation">
+                                <div class="card-shadow-primary card-border mb-3 profile-responsive-sm card">
+                                    <div class="dropdown-menu-header">
+                                        <div class="dropdown-menu-header-inner bg-vicious-stance">
+                                            <div class="menu-header-image opacity-1"
+                                                style="background-image: url('assets/images/dropdown-header/abstract1.jpg');"></div>
+                                            <div class="menu-header-content btn-pane-right">
+                                                <div class="avatar-icon-wrapper mr-3 avatar-icon-xl btn-hover-shine">
+                                                    <div class="avatar-icon rounded-circle">
+                                                        <img src="assets/images/avatars/13.jpg" alt="Avatar 5">
+                                                    </div>
+                                                </div>
+                                                <div>
+                                                    <h5 class="menu-header-title">Abdul Hasan</h5>
+                                                    <h6 class="menu-header-subtitle">SMKN 1 PROBOLINGGO</h6>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                    </div>
+                    <table class="align-middle mb-0 table table-borderless table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th class="text-center">No.</th>
+                                <th>Nama</th>
+                                <th class="text-center">Tanggal Donasi</th>
+                                <th class="text-center">Nominal</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="text-center text-muted">1.</td>
+                                <td>
+                                    <div class="widget-content p-0">
+                                        <div class="widget-content-wrapper">
+                                            <div class="widget-content-left mr-3">
+                                                <div class="widget-content-left">
+                                                    <img width="40" class="rounded-circle" src="assets/images/avatars/4.jpg" alt="">
+                                                </div>
+                                            </div>
+                                            <div class="widget-content-left flex2">
+                                                <div class="widget-heading">John Doe</div>
+                                                <div class="widget-subheading opacity-7">Amal Jariyah untuk Siswa Siswi Yatim Piatu di SMKN 13 Malang</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="text-center">20 maret 2020</td>
+                                <td class="text-center">
+                                    <div class="badge">20.000</div>
+                                </td>
+                                <td class="text-center" style="width: 150px;">
+                                    <div class="pie-sparkline">Sudah Bayar</div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="text-center text-muted">2.</td>
+                                <td>
+                                    <div class="widget-content p-0">
+                                        <div class="widget-content-wrapper">
+                                            <div class="widget-content-left mr-3">
+                                                <div class="widget-content-left">
+                                                    <img width="40" class="rounded-circle" src="assets/images/avatars/3.jpg" alt="">
+                                                </div>
+                                            </div>
+                                            <div class="widget-content-left flex2">
+                                                <div class="widget-heading">Ruben Tillman</div>
+                                                <div class="widget-subheading opacity-7">Bedah Sekolah dari kardus di pelosok Desa Kabupaten Probolinggo</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="text-center">21 Januari 2020</td>
+                                <td class="text-center">
+                                    <div class="badge">500.000</div>
+                                </td>
+                                <td class="text-center" style="width: 150px;">
+                                    <div class="pie-sparkline">Belum Bayar</div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="text-center text-muted">3.</td>
+                                <td>
+                                    <div class="widget-content p-0">
+                                        <div class="widget-content-wrapper">
+                                            <div class="widget-content-left mr-3">
+                                                <div class="widget-content-left">
+                                                    <img width="40" class="rounded-circle" src="assets/images/avatars/2.jpg" alt="">
+                                                </div>
+                                            </div>
+                                            <div class="widget-content-left flex2">
+                                                <div class="widget-heading">Elliot Huber</div>
+                                                <div class="widget-subheading opacity-7">Terdampak Corona. Bantu Wahyudi Siswa Yang Putus Sekolah
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="text-center">14 April 2020</td>
+                                <td class="text-center">
+                                    <div class="badge">25.000</div>
+                                </td>
+                                <td class="text-center" style="width: 150px;">
+                                    <div class="pie-sparkline">Belum Bayar</div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td class="text-center text-muted">4.</td>
+                                <td>
+                                    <div class="widget-content p-0">
+                                        <div class="widget-content-wrapper">
+                                            <div class="widget-content-left mr-3">
+                                                <div class="widget-content-left">
+                                                    <img width="40" class="rounded-circle" src="assets/images/avatars/1.jpg" alt=""></div>
+                                            </div>
+                                            <div class="widget-content-left flex2">
+                                                <div class="widget-heading">Vinnie Wagstaff</div>
+                                                <div class="widget-subheading opacity-7">Ika siswi smk 20 malang mengalami patah tulang akibat kecelakaan.Mari ringankan biaya pengobatan ika.</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="text-center">30 Desember 2020</td>
+                                <td class="text-center">
+                                    <div class="badge">1.000.000</div>
+                                </td>
+
+                                <td class="text-center" style="width: 150px;">
+                                    <div class="pie-sparkline">Sudah Bayar</div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
